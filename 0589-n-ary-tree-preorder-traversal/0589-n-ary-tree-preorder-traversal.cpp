9e2cd5f127932730e1865758165c7a16ca91ebea
Prateek Singh <prateeksingh9913@gmail.com>
2026-07-09 16:34:44 +0530

    Time: 17 ms (23.73%), Space: 15.7 MB (38.89%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void dfs(Node* root, vector<int>& ans) {
+        if (root == NULL) return;
+
+        ans.push_back(root->val);
+
+        for (Node* child : root->children) {
+            dfs(child, ans);
+        }
+    }
+
+    vector<int> preorder(Node* root) {
+        vector<int> ans;
+        dfs(root, ans);
+        return ans;
+    }
+};
